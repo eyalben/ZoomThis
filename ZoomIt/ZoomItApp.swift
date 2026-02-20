79c69718eb1fa6d41e3b72dda9d8b5e68452a9cc
Eyal <eyalben@gmail.com>
2026-02-20 10:09:14 +0200

    Initial Commit

---
+++ ZoomIt/ZoomItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomItApp.swift
+//  ZoomIt
+//
+//  Created by Eyal on 20/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ZoomItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
